e143b681dfda44a747d1ae9bfc9767cfc8562ac3
jerry_glb <goodEvening_glb@atguigu.com>
2020-01-01 12:48:15 +0800

    jap

---
+++ spring-boot-06-data-jap/src/main/java/com/atuigu/springboot/SpringBoot06DataJapApplication.java
@@ -0,0 +1,13 @@
+package com.atuigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot06DataJapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot06DataJapApplication.class, args);
+    }
+
+}
